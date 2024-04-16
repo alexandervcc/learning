81a76f6526a45c11a5bf049b2b398fb24f9a7f37
alexandervcc <alex.charco.25@gmail.com>
2024-04-15 19:01:03 -0500

    Register View with Firebase

--- dart/flutter_foundamentals/lib/main.dart
+++ dart/flutter_foundamentals/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -11,8 +15,32 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //Proxy to obtain a single store for text across children components
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  // late: same as lateinit on kotlin
+
+  @override
+  void initState() {
+    super.initState();
+    _email = TextEditingController();
+    _password = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _email.dispose();
+    _password.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +50,50 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.amber,
       ),
       // center vertically/horizontally the widget
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            print("Mijotron XDDD");
-          },
-          child: const Text("Register"),
-        ),
-      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          //this column will be shown once the future has been completed
+          builder: (context, snapshot) {
+            //snapshot: state of the object at the moment, it is the result of the future defined
+            // get the result of the future succes vs faile
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(hintText: "email"),
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                        hintText: "password",
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        print(userCredential);
+                      },
+                      child: const Text("Register"),
+                    ),
+                  ],
+                );
+              default:
+                return const Text('Loading');
+            }
+          }),
     );
   }
 }
