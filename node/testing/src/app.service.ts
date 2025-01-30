4bb6a060241a1e0a35c0771a7d9d7fb795ff49ee
alexandervcc <alex.charco.25@gmail.com>
2025-01-29 21:23:21 -0500

    Full example to generate and publish the server client

--- node/testing/src/app.service.ts
+++ node/testing/src/app.service.ts
@@ -4,6 +4,7 @@ import { AppReqDto, AppResDto } from './dto/app.dto';
 @Injectable()
 export class AppService {
   getHello(dto: AppReqDto): AppResDto {
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>dto:', JSON.stringify(dto));
     return {
       message: `Hello ${dto.name}!`,
     };
